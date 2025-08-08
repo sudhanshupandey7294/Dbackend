e196873bd9b229af6c550024b060d0aea5e18c54
sudhanshupandey7294 <sudhanshupandey738@gmail.com>
2025-08-08 23:56:54 +0530

    d back

--- server.js
+++ server.js
@@ -21,5 +21,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB Connected'))
 .catch((err) => console.log('DB Error:', err));
 
+app.get('/', (req, res) => {
+  res.send('D-SERVICES Backend is Live 🚀');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
